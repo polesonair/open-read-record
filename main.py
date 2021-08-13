b240f4f59abfd3104a536eb8cc361aa50536147a
polesonair <88142197+polesonair@users.noreply.github.com>
2021-08-13 09:30:26 +0300

    task 3

--- main.py
+++ main.py
@@ -1,51 +1,27 @@
-from pprint import pprint
+import os
+import operator
 
+files_list = os.listdir(path='files/')
+files_dct = {}
 
-# №1
-def create_cook_book(input_file_name):
-    cook_book = {}
+# Добавляем к именам файлов путь к папке
+for i in range(len(files_list)):
+    files_list[i] = 'files/' + files_list[i]
 
-    try:
-        with open(input_file_name, encoding='utf-8') as f: # считали файл, разбили на строки, записали в список
-            lst = [line.strip() for line in f]
-        for i, j in enumerate(lst):
-            if j.isdigit(): # метод для поиска числе, если перебираем число, то берем название предыдущего блюда
-                cook_book[lst[i-1]] = [] # собираем их списк
+#  Получаем имена файлов, число строк в файлах, помещаем в словарь имя_файла:число строк
+for read_file in files_list:
+    with open(read_file, 'r', encoding='utf-8') as current_file:
+        current_text = current_file.readlines()
+    count = len(current_text)
+    files_dct[read_file] = count
 
-                for s in lst[i+1:i+int(j)+1]:
-                    ingredient_name = s.split('|')[0]
-                    quantity = int(s.split('|')[1])
-                    measure = s.split('|')[2]
+files_sorted = sorted(files_dct.items(), key=operator.itemgetter(1))
 
-                    cook_book[lst[i-1]].append({'ingredient_name':ingredient_name,
-                                                'quantity':quantity,
-                                                'measure':measure})
-        return cook_book
-
-    except FileNotFoundError:
-        return(f'Файл: {input_file_name} не найден.')
-    except Exception as error:
-        return f'Ошибка - {error}'
-
-
-# №2
-def get_shop_list_by_dishes(dishes, cooking_book, person_count):
-    ingredient_dict = {}
-
-    for key in cooking_book.keys ():
-        for dish in dishes:
-            if key == dish:
-                for dictionary in cooking_book[key]:
-                    ingredient_name = dictionary['ingredient_name']
-
-                    try:
-                        ingredient_dict[ingredient_name]['quantity'] += (dictionary['quantity'] * person_count)
-                    except:
-                        ingredient_dict[ingredient_name] = {'measure': dictionary['measure'],
-                                              'quantity': dictionary['quantity'] * person_count}
-
-    return ingredient_dict
-
-pprint(create_cook_book('recipes.txt'))
-print ('----------------------------')
-pprint(get_shop_list_by_dishes(['Омлет', 'Фахитос'], create_cook_book('recipes.txt'), 2))
+with open('files/out.txt', 'w', encoding='utf-8') as file_out:
+    for i in files_sorted:
+        file_out.write(i[0][6:]+'\n')  # Удаляем из имени путьк  фалу и записываем в новый файл
+        file_out.write(str(i[1])+'\n')  # Записываем число строк в текущем файле
+        # Читаем стрки из файлов и записываем в ноый файл
+        with open(i[0], 'r', encoding='utf-8') as reading_file:
+            for line in reading_file:
+                file_out.write(line)
